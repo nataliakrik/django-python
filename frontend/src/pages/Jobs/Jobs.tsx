debfe99252b2e6635a159bfd54c800f3134fe9ac
soc9999 <socrates.papargiris@gmail.com>
2024-09-16 19:23:28 +0300

    some navigation bars

--- frontend/src/pages/Jobs/Jobs.tsx
+++ frontend/src/pages/Jobs/Jobs.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom'
 
 ReactDOM.render(
     <React.StrictMode>
+        <div className="top-bar"> 
+        <a href="./home" className="top-bar-link">Home Page</a>
+        <a href="./Jobs/index.html" className="top-bar-link">Jobs</a>
+        <a href="./Messages/index.html" className="top-bar-link">Messages</a>
+        <a href="./MyNetwork/index.html" className="top-bar-link">My Network</a>
+        <a href="./Notifications/index.html" className="top-bar-link">Notifications</a>
+        <a href="./profile" className="top-bar-link">Profile</a>
+        <a href="./settings" className="top-bar-link">Settings</a>
+      </div>
         <h1>Jobs</h1>
     </React.StrictMode>,
     document.getElementById('root')//dont delete this
