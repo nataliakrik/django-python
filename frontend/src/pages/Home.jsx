ba4141e5129d90a3706668a760c1a317187a1766
nataliakrik <nataliakrik2002@gmail.com>
2024-09-20 01:59:14 +0300

    photo profile

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,48 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'; 
 import api from "../api"; 
-import Note from '../components/Note'
+//import Note from '../components/Note'
 import "../styles/Note.css"
 
 function Home(){
-    const [notes , setNotes] = useState([]);
-    const [content , setContent]= useState("");
-    const [title , setTitle]= useState("");
 
+    const [users, setUsers] = useState([]);  // Initialize users as an empty array
+    const [loading, setLoading] = useState(true);  // State to handle loading
+    const [error, setError] = useState(null);  // State to handle errors
+    const token = localStorage.getItem('access');  // Assume token is stored in localStorage after login
+
+    useEffect(() => {
+        const fetch_Username_photo = async () => {
+            try {
+                // Calling api.get with 'api/usernameAndPhoto/' path and the token authorization to gain access to the username and photo
+                const response = await api.get('api/usernameAndPhoto/', {headers: {Authorization: `Bearer ${token}`,},});
+
+                // Saving the response data
+                const usersData = response.data;
+                setUsers(usersData);
+                setLoading(false);  // Stop loading
+            } catch (error) {
+                console.error('Error fetching users:', error);  // Log error
+                setError('Failed to fetch users');
+                setLoading(false);  // Stop loading
+            }
+        };
+    
+        fetch_Username_photo();  // Call the function 
+        
+    }, [token]);
+    
+    // Display a loading message if data is still being fetched
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    // Display an error message if there was an issue fetching data
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    /*
     useEffect(() => {
         getNotes();
     }, [])
@@ -47,7 +81,9 @@ function Home(){
         }).catch((err) => alert(err));
     };
 
-    return <div>
+    */
+    
+    return <div className="home">
         <div className="top-bar"> 
             <Link to ="/home" className="top-bar-link">Home page</Link>
             <Link to ="/jobs" className="top-bar-link">Jobs</Link>
@@ -66,8 +102,24 @@ function Home(){
         {/* -πλαισιο με το ονομα + link στο προφιλ του χρηστη
         , link για το δικτυο του
         -απο κατω στο κεντρο εχει αρθρα */}
-
-        
+        <div className="profile">
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        user details:
+                        <br />
+                        profile picture : 
+                        <img
+                            src={user.profile_picture}
+                            alt={user.username}
+                            style={{ width: '100px', height: '100px' }}
+                            />
+                        <br />
+                        <span style={{ color: 'black' }}>username: {user.username}</span>
+                  </li>
+                ))}
+            </ul>
+        </div>
 
 
 
--- frontend/src/pages/MyNetwork.jsx
+++ frontend/src/pages/MyNetwork.jsx
@@ -1,45 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import api from '../api';
-import '../styles/messages.css';
 
 function MyNetwork(){
-    const [users, setUsers] = useState([]);  // Initialize users as an empty array
-    const [loading, setLoading] = useState(true);  // State to handle loading
-    const [error, setError] = useState(null);  // State to handle errors
-    const token = localStorage.getItem('access');  // Assume token is stored in localStorage after login
-
-    useEffect(() => {
-        const fetch_Username_photo = async () => {
-            try {
-                // Calling api.get with 'api/usernameAndPhoto/' path and the token authorization to gain access to the username and photo
-                const response = await api.get('api/usernameAndPhoto/', {headers: {Authorization: `Bearer ${token}`,},});
-
-                // Saving the response data
-                const usersData = response.data;
-                setUsers(usersData);
-                setLoading(false);  // Stop loading
-            } catch (error) {
-                console.error('Error fetching users:', error);  // Log error
-                setError('Failed to fetch users');
-                setLoading(false);  // Stop loading
-            }
-        };
-    
-        fetch_Username_photo();  // Call the function 
-        
-    }, [token]);
-    
-    // Display a loading message if data is still being fetched
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    // Display an error message if there was an issue fetching data
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
     return <div>
         <div className="top-bar">          
                     <Link to ="/home" className="top-bar-link">Home page</Link>
@@ -59,18 +21,6 @@ function MyNetwork(){
         -ενα χωρο εμφανιζονται τα φιλικα προφιλ
         ανα ζευγαρια το ενα κατω απο το αλλο */}
         <h1>MyNetwork page</h1>
-            <ul>
-                {users.map(user => (
-                    <li key={user.id}>
-                        <img
-                            src={user.profile_picture}
-                            alt={user.username}
-                            style={{ width: '100px', height: '100px' }}
-                            />
-                        <span style={{ color: 'black' }}>{user.username}</span>
-                  </li>
-                ))}
-            </ul>
     </div>
 }
 
