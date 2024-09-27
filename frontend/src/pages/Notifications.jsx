3d1ab9df126007d9420fd9cbec7c077a75af7435
soc9999 <socrates.papargiris@gmail.com>
2024-09-27 18:15:27 +0300

    minor changes

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,31 +1,59 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Notifications(){
-    return <div>
-        <div className="top-bar">          
-                    <Link to ="/home" className="top-bar-link">Home page</Link>
-                    <Link to ="/jobs" className="top-bar-link">Jobs</Link>
-                    <Link to ="/messages" className="top-bar-link">Messages</Link>
-                    <Link to ="/mynetwork" className="top-bar-link">My Network</Link>
-                    <Link to ="/notifications" className="top-bar-link">Notifications</Link>
-                    <Link to ="/profile" className="top-bar-link">Profile</Link>
-                    <Link to ="/settings" className="top-bar-link">Settings</Link>
-        </div>
-        <h1>Notifications ή ειδοποιησεις</h1>
+function Notifications() {
+  // fetch from api:
+  // ολα τα αιτηματα συνδεσης, τα αρθρα που εγιναν like
+  // , απο ποιον, τα αρθρα που εγιναν comment, απο ποιον
+  // + ποιο ειναι το comment
 
-        {/* fetch from api:
-        ολα τα αιτηματα συνδεσης, τα αρθρα που εγιναν like
-        , απο ποιον, τα αρθρα που εγιναν comment, απο ποιον
-        + ποιο ειναι το comment */}
+  return (
+    <div>
+      <div className="top-bar">
+        <Link to="/home" className="top-bar-link">
+          Home page
+        </Link>
+        <Link to="/jobs" className="top-bar-link">
+          Jobs
+        </Link>
+        <Link to="/messages" className="top-bar-link">
+          Messages
+        </Link>
+        <Link to="/mynetwork" className="top-bar-link">
+          My Network
+        </Link>
+        <Link to="/notifications" className="top-bar-link">
+          Notifications
+        </Link>
+        <Link to="/profile" className="top-bar-link">
+          Profile
+        </Link>
+        <Link to="/settings" className="top-bar-link">
+          Settings
+        </Link>
+      </div>
+      <h1 className="center">Notifications</h1>
 
-        {/* Το front εκτυπωνει:
+      {/* Το front εκτυπωνει:
         -(πανω μερος της σελιδας) 
-        τα αιτηματα συνδεσης αλλων χρηστων+συνδεσμοι για προφιλ
+        τα αιτηματα συνδεσης αλλων χρηστων + συνδεσμοι για προφιλ
         -(κατω μερος της σελιδας)
         likes, comments  σε αρθρα */}
 
+      {/* πανω μερος σελιδας-αιτησεις αλλων χρηστων */}
+      <div>
+        <h2>Friend requests:</h2>
+        <ul>
+          <li>
+            <Link to="/otherprofile">name1:</Link> accept / deny
+          </li>
+          <li>name2:</li>
+        </ul>
+      </div>
 
+      {/*κατω μερος σελιδας-likes, comments*/}
+      <div></div>
     </div>
+  );
 }
 
-export default Notifications
+export default Notifications;
